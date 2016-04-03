47bfdb4ed3ac71a95f62dc2955bcd955e6f1573b
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2016-04-02 22:13:45 -0500

    App complete with full functionality and styling.

--- index.ios.js
+++ index.ios.js
@@ -11,52 +11,79 @@ var {
 var StopWatch = React.createClass({
   getInitialState: function() {
     return {
-      timeElapsed: null  
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: []
     }
   },
   render: function() {
-    return <View style={[styles.container, this.border('#44ee77')]}>
-      <View style={[styles.header, this.border('#7744ee')]}>
-        <View style={[styles.timerWrap, this.border('#99dd77')]}>
-          <Text>
+    return <View style={[styles.container]}>
+      <View style={[styles.header]}>
+        <View style={[styles.timerWrap]}>
+          <Text style={styles.timer}>
             {formatTime(this.state.timeElapsed)}
           </Text>
         </View>
-        <View style={[styles.buttonsWrap, this.border('#77dd99')]}>
+        <View style={[styles.buttonsWrap]}>
           {this.startStopButton()}
           {this.lapButton()}
         </View>
       </View>
-      <View style={[styles.footer, this.border('#ee7744')]}>
-        <Text>
-          This is for the laps.
-        </Text>
+      <View style={[styles.footer]}>
+        {this.laps()}
       </View>
     </View>
   },
+  laps: function() {
+    return this.state.laps.map(function(time, index){
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
+          #{index + 1} : {formatTime(time)}
+        </Text>
+      </View>
+    });
+  },
   startStopButton: function() {
-    return <TouchableHighlight underlayColor="gray" onPress={this.handleStartPress}>
+    var style = this.state.running ? styles.stopButton : styles.startButton;
+    return <TouchableHighlight underlayColor="gray" onPress={this.handleStartPress} style={[styles.button, style]}>
       <Text>
-        Start
+        {this.state.running ? 'Stop' : 'Start'}
       </Text>
     </TouchableHighlight>
   },
-  handleStartPress: function() {
-    console.log('start button pressed');
-    var startTime = new Date();
+  handleLapPress: function() {
+    var lap = this.state.timeElapsed;
 
-    setInterval(() => {
+    this.setState({
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap]) 
+    });
+  },
+  handleStartPress: function() {
+    if(this.state.running) {
+      clearInterval(this.interval);
       this.setState({
-        timeElapsed: new Date() - startTime
+        running: false
+      })
+      return;
+    }
+
+    this.setState({startTime: new Date()});
+
+    this.interval = setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - this.state.startTime,
+        running: true
       });
     }, 30);
   },
   lapButton: function() {
-    return <View>
+    return <TouchableHighlight underlayColor='gray' onPress={this.handleLapPress} style={styles.button}>
       <Text>
         Lap
       </Text>
-    </View>
+    </TouchableHighlight>
   },
   border: function(color) {
     return {
@@ -75,18 +102,51 @@ var styles = StyleSheet.create({
     flex: 1
   },
   footer: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#dfdfdf'
   },
   timerWrap: {
     flex: 6,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#f9f9f9'
   },
   buttonsWrap: {
     flex: 4,
     flexDirection: 'row',
     justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#efefef'
+  },
+  timer: {
+    color: '#575757',
+    fontSize: 50
+  },
+  button: {
+    backgroundColor: '#f7f7f7',
+    borderWidth: 2,
+    borderRadius: 50,
+    borderColor: '#999',
+    height: 100,
+    width: 100,
+    justifyContent: 'center',
     alignItems: 'center'
+  },
+  startButton: {
+    borderColor: '#78BB45'
+  },
+  stopButton: {
+    borderColor: '#BB7845'
+  },
+  lap: {
+    borderWidth: 1,
+    borderColor: '#efefef',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    padding: 5
+  },
+  lapText: {
+    fontSize: 25
   }
 })
 
